5728c29aea265d8fb4b9124b25e0af1dc69645b5
Richab-star <112754495+Richab-star@users.noreply.github.com>
2022-09-16 13:08:17 +0200

    frequence of your first name and french historical events of your birth year
    this new version allows acces to some major french historical events of your birth year.

--- prenom/script-prenoms3.js
+++ prenom/script-prenoms3.js
@@ -1,30 +1,40 @@
 function clique()
 {
+   document.getElementById("zoneTexte").innerHTML = "";
+   document.getElementById("visu1").innerHTML = "";
+   document.getElementById("visu2").innerHTML = "";
+   document.getElementById("visu3").innerHTML = "";
    $.get("prenoms.json", function(objetJs){mesCalculs(objetJs);});
 }
 
 
-function mesCalculs(data)
+function mesCalculs(myJson)
 {
    const prenom = document.getElementById("nom").value;
    const age = document.getElementById("age").value;
-   var notredate = new Date()
-   var cette_annee = notredate.getFullYear()
-   const naissance = parseInt(cette_annee) - parseInt(age);
-   const tailleJson = data.length;
-   var genre = ["","garçons nommés","filles nommées"];
+   const tailleJson = myJson.length;
    var garconAnnee = [];
    var garconNombre = [];
    var filleAnnee = [];
    var filleNombre = [];
+   var resultat;
+   
+   if ( prenom == "" || age === ""){
+      document.getElementById("visu1").innerHTML = 
+      "Entrez vos données !!";
+      return
+   } else {
+      document.getElementById("visu1").innerHTML = 
+      "Fréquence de ce prénom au cours des années :"
+   };
 
    for (var i=0; i<313346; i++)
   {
-     if (data[i].preusuel == prenom.toUpperCase())
+     if (myJson[i].preusuel == prenom.toUpperCase())
       {
-         if (data[i].annais != "XXXX")
-            {  garconAnnee.push(data[i].annais);
-               garconNombre.push(data[i].nombre);
+         if (myJson[i].annais != "XXXX")
+            {  garconAnnee.push(myJson[i].annais);
+               garconNombre.push(myJson[i].nombre);
             }
             else {break ;}
       };
@@ -32,11 +42,11 @@ function mesCalculs(data)
    
   for (var j=313347; j<tailleJson; j++)
   {
-      if (data[j].preusuel == prenom.toUpperCase())
+      if (myJson[j].preusuel == prenom.toUpperCase())
       {
-         if (data[j].annais != "XXXX")
-            {  filleAnnee.push(data[j].annais);
-               filleNombre.push(data[j].nombre);
+         if (myJson[j].annais != "XXXX")
+            {  filleAnnee.push(myJson[j].annais);
+               filleNombre.push(myJson[j].nombre);
             }
             else {break ;}
       };
@@ -44,7 +54,7 @@ function mesCalculs(data)
      
   if (garconAnnee.length == 0 && filleAnnee == 0)
    {
-      var resultat = "Aucune naissance avec le prénom "+prenom+" en France depuis 1900 !";
+      resultat = "Aucune naissance avec le prénom "+prenom+" en France depuis 1900 !";
       document.getElementById("zoneTexte").innerHTML = resultat;
    };
 
@@ -66,7 +76,7 @@ function mesCalculs(data)
         text:"Le prénom " + prenom,
         font: {
           family: 'Arial',
-          size: 24
+          size: 28
          },
         xref: 'paper',
         x: 0.05,
@@ -94,4 +104,65 @@ function mesCalculs(data)
    };
    
    Plotly.newPlot('monGraphe', data, layout);
+
+   
+   var notredate = new Date();
+   var cette_annee = notredate.getFullYear();
+   const naissance = parseInt(cette_annee) - parseInt(age);
+
+   // Recherche Nbr garcon/filles avec ce prenom cette année :
+   var nbrNomGa;
+   var nbrNomFi;
+   var index;
+   var resultat;
+   index = garconAnnee.indexOf(naissance);
+   if(index < 0){nbrNomGa = 0}
+   else{nbrNomGa = garconNombre[index]};
+   index = filleAnnee.indexOf(naissance);
+   if(index < 0){nbrNomFi = 0}
+   else{nbrNomFi = filleNombre[index]};
+   resultat = "En " + naissance + " , l'année de votre naissance, ";
+   resultat += nbrNomGa + " garçons et " + nbrNomFi + " filles ";
+   resultat += "avaient reçu le prénom " + prenom + " en France.";
+   $("#visu2").append(
+      resultat).
+      css("color", "blue").
+      css("font-size", "16px"
+   );
+
+   // Recherche noms garcons et filles les plus fréquents de l'année :
+   var maxNomGa = 0;
+   var maxNomFi = 0;
+   var nomMaxGa = "";
+   var nomMaxFi = "";
+   var resultat2 = "";
+   for (var i=0; i<313346; i++){
+      if(myJson[i].annais == naissance && myJson[i].nombre > maxNomGa){
+         maxNomGa = myJson[i].nombre;
+         nomMaxGa = myJson[i].preusuel;
+      };
+   }
+   for (var j=313347; j<tailleJson; j++){
+      if(myJson[j].annais == naissance && myJson[j].nombre > maxNomFi){
+         maxNomFi = myJson[j].nombre;
+         nomMaxFi = myJson[j].preusuel;
+      };
+   }
+   resultat2 += "Mais le prenom de garçon le plus fréquent cette année là ";
+   resultat2 += "était " + nomMaxGa + " , et le plus fréquent chez les filles ";
+   resultat2 += "était " + nomMaxFi + ".";
+   $("#visu2").append("\n" + resultat2);
+
+   // Recherche des évènements de l'année de naissance :
+   $.get('evenements.txt', function(mesEvenements) {
+      var lines = mesEvenements.split("\r\n");
+      var numeroLignes = naissance - 1900;
+      var maLigne = lines[numeroLignes];
+      $("#visu3").text(
+         "Voici des évènements de " + naissance + " , l'année de votre naissance :").
+         css("color", "red").
+         css("font-size", "24px"
+      );
+      $("#zoneTexte").append(maLigne); 
+   }, 'text');
 }
